bd961d20f17e6d574bbef3d0c68360b8ac773af9
nimsajj <nimsaj.dev@gmail.com>
2021-01-11 14:01:39 +0100

    integrate Appbar and container from material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container, CssBaseline } from "@material-ui/core";
+
 import Navbar from "./Navbar";
 
 const Home = () => <div>Home page</div>;
 const Login = () => <div>Login page</div>;
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <CssBaseline />
+    <Navbar />
+    <Container maxWidth="sm">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Container>
+  </Router>
+);
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Menu as MenuIcon } from "@material-ui/icons";
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    color: theme.palette.common.white,
+    textDecoration: "none",
+  },
+  offset: theme.mixins.toolbar,
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/" className={classes.link}>
+              Books
+            </Link>
+          </Typography>
+
+          <Button color="inherit">
+            <Link to="/login" className={classes.link}>
+              Login
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
+    </>
+  );
+};
 
 export default Navbar;
